3f7f953b2af622e54295f9076073d6c9fe04603e
jfarrellhfx <jfarrellhfx@gmail.com>
2021-01-28 18:07:44 -0400

    t

---
+++ _site/labbook/makeindex.py
@@ -0,0 +1,16 @@
+import os
+filenames = os.listdir()
+
+
+with open("index.md", "w") as g:
+    g.write("---\nlayout: page\ntitle: Lab Book\n---\n")
+    g.write("<ul>")
+
+    for filename in filenames:
+        if filename != "index.md" and filename != "makeindex.py" and filename[0] != ".":
+            if "." in filename:
+                line = "<li><a href = \"{}\">{}</a></li>\n".format(filename, filename)
+            else:
+                line = "<li><a href = \"{}\"><i class=\"material-icons\" style=\"color:#bbb\">folder</i> {}</a></li>\n".format(filename, filename)
+            g.write(line)
+    g.write("</ul>")
